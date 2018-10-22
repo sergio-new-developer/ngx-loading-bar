8adc80b1036fdddb4551bc7b90147e01df698212
sergio-new-developer <39557128+sergio-new-developer@users.noreply.github.com>
2018-10-22 20:56:39 +0300

    no spinner plz

--- packages/core/src/loading-bar.component.ts
+++ packages/core/src/loading-bar.component.ts
@@ -5,9 +5,6 @@ import { LoadingBarService } from './loading-bar.service';
   selector: 'ngx-loading-bar',
   template: `
     <ng-container *ngIf="(value !== null ? value : loader.progress$|async) as progress">
-      <div id="loading-bar-spinner" *ngIf="includeSpinner" [style.color]="color">
-        <div [style.width]="diameter" [style.height]="diameter" class="spinner-icon"></div>
-      </div>
       <div id="loading-bar" *ngIf="includeBar" [style.color]="color">
         <div class="bar" [style.background]="color" [style.height]="height" [style.width]="progress + '%'">
           <div class="peg" [style.height]="height"></div>
